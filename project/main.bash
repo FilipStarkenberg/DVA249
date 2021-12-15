66de51c7949263f4ff8ee15772accc954a91aaa7
Filip Starkenberg <filip.starkenberg@gmail.com>
2021-12-15 15:32:50 +0100

    Added some outline and added some menues

--- project/main.bash
+++ project/main.bash
@@ -1,19 +1,92 @@
 #!/bin/bash
 
-mainmenu(){
-    clear
-    echo "Welcome to this application!"
-    echo 
-    echo "What do you want to do?"
-    echo "[n] Network information... "
-    echo "[u] User management... "
-    echo "[g] Group management... "
-    echo "[d] Directory management... "
-    read -p '> ' mainselect
+#Todo:
+# Application name layout
+# Colors to sub menu names
+# 
+#
+#
+#
+
+
+netinfo(){
+    while true; do
+        clear
+        
+        echo "Network information"
+        echo 
+        echo "What do you want to do?"
+        echo
+        echo "[n] Display computer Name (host name)"
+        echo "[i] Display IP address. "
+        echo "[m] Display MAC address. "
+        echo "[g] Display Gateway. "
+        echo "[s] Display interface Status."
+        echo "[e] Go back. "
+        read -p '> ' selection
+
+        if [[ "$selection" == "n" ]]; then
+            echo
+        elif [[ "$selection" == "i" ]]; then
+            echo
+        elif [[ "$selection" == "m" ]]; then
+            echo
+        elif [[ "$selection" == "g" ]]; then
+            echo
+        elif [[ "$selection" == "s" ]]; then
+            echo
+        elif [[ "$selection" == "e" ]]; then
+            break
+        else
+            echo "invalid input."
+        fi
+        read -p "Press enter to return to menu..." temp
+    done
 }
 
-while true; do
-    mainmenu
-    echo $mainselect
-    read -p "Press enter to continue..." temp
-done
+usermanage(){
+
+}
+
+groupmanage(){
+
+}
+
+dirmanage(){
+
+}
+
+mainmenu(){
+    while true; do
+        clear
+
+        echo "Welcome to this application!"
+        echo 
+        echo "What do you want to do?"
+        echo
+        echo "[n] Network information... "
+        echo "[u] User management... "
+        echo "[g] Group management... "
+        echo "[d] Directory management... "
+        echo "[e] Exit."
+        read -p '> ' selection
+
+        if [[ "$selection" == "n" ]]; then
+            netinfo
+        elif [[ "$selection" == "u" ]]; then
+            usermanage
+        elif [[ "$selection" == "g" ]]; then
+            groupmanage
+        elif [[ "$selection" == "d" ]]; then
+            dirmanage
+        elif [[ "$selection" == "e" ]]; then
+            break
+        else
+            echo "invalid input."
+        fi
+        read -p "Press enter to return to menu..." temp
+    done
+}
+
+#Main entry point for the application
+mainmenu
